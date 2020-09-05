c886a40d53c48560db2b14eddbe8d2db947b25ae
Your Name <you@example.com>
2020-09-05 09:10:24 -0600

    commit

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 echo 'The following Maven command installs your Maven-built Java application'
 echo 'into the local Maven repository, which will ultimately be stored in'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
